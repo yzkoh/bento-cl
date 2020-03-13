62a568756fd0a61797a5f0612ac3fbd775e9e827
YZ <yz.goo.gl@gmail.com>
2020-03-13 22:18:42 +0000

    Remove verbose and update get device info

--- src/util/clUtils/core.c
+++ src/util/clUtils/core.c
@@ -1,5 +1,4 @@
 #include <CL/cl_platform.h>
-#include <string.h>
 
 #include "core.h"
 
@@ -7,4 +6,10 @@ void getPlatforms(cl_platform_id *platforms, cl_int *platformCount){
     clGetPlatformIDs(0, NULL, platformCount);
     platforms = (cl_platform_id *) malloc(sizeof(cl_platform_id) * (*platformCount) );
     clGetPlatformIDs(*platformCount, platforms, NULL);
+}
+
+void getDevices(cl_platform_id platform, cl_device_id *devices, cl_uint *deviceCount){
+    clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, NULL, deviceCount);
+    devices = (cl_device_id *) malloc(sizeof(cl_device_id) * (*deviceCount) );
+    clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, *deviceCount, devices, NULL);
 }
