91f795bb5aaa2029f02178c821092d7b002b52bc
YZ <yz.goo.gl@gmail.com>
2020-03-13 21:21:48 +0000

    Exclude verbose

--- src/util/clUtils/core.c
+++ src/util/clUtils/core.c
@@ -7,9 +7,4 @@ void getPlatforms(cl_platform_id *platforms, cl_int *platformCount){
     clGetPlatformIDs(0, NULL, platformCount);
     platforms = (cl_platform_id *) malloc(sizeof(cl_platform_id) * (*platformCount) );
     clGetPlatformIDs(*platformCount, platforms, NULL);
-
-    #ifdef VERBOSE
-    if(*platformCount == 0) printf("No OpenCL platforms available.\n");
-    else printf("Number of OpenCL platforms: %d\n",*platformCount);
-    #endif
 }
