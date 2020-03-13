f822f04ec51f57fc2669dddc6d3bd75f702e9eb6
YZ <yz.goo.gl@gmail.com>
2020-03-13 22:18:26 +0000

    Update verbose

---
+++ src/util/clUtils/verbose.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "verbose.h"
+
+// Get device details: field label
+char *deviceInfoFieldLabel(cl_uint field){
+    switch(field){
+        case CL_DEVICE_NAME: return("Devices");
+        case CL_DEVICE_VERSION: return("Hardware version");
+        case CL_DRIVER_VERSION: return("Software version");
+        case CL_DEVICE_OPENCL_C_VERSION: return("OpenCL C version");
+        case CL_DEVICE_TYPE: return("Device type");
+        case CL_DEVICE_MAX_COMPUTE_UNITS: return("Parallel compute units");
+        case CL_DEVICE_MAX_WORK_GROUP_SIZE: return("Max work group size");
+        case CL_DEVICE_MAX_WORK_ITEM_SIZES: return("Max work items");
+        case CL_DEVICE_MAX_CLOCK_FREQUENCY: return("Max clock frequency");
+        case CL_DEVICE_GLOBAL_MEM_SIZE: return("Device global memory");
+        case CL_DEVICE_NATIVE_VECTOR_WIDTH_DOUBLE: return("Double precision supported");
+        default: return("Unknown");
+    }
+}
---
+++ src/util/clUtils/verbose.h
@@ -0,0 +1,11 @@
+#ifndef BENTOCL_VERBOSE_H
+#define BENTOCL_VERBOSE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <CL/cl.h>
+#include <stdbool.h>
+
+char *deviceInfoFieldLabel(cl_uint field);
+
+#endif
