13f71ca6038b359e521037c5163ef0afecc1825e
YZ <yz.goo.gl@gmail.com>
2020-03-14 01:43:00 +0000

    Move display devices to interface.c

---
+++ src/util/clUtils/interface.c
@@ -0,0 +1,49 @@
+#include <CL/cl_platform.h>
+
+#include "core.h"
+#include "verbose.h"
+
+void listAllDevices(){
+    
+    char output[100];
+
+    verbose(""); // new line
+
+    // Get platforms
+    cl_platform_id *platforms;
+    cl_int platformCount;
+
+    platforms = getPlatforms(&platformCount);
+
+    if(platformCount==0) verbose("No OpenCL platform available.");
+    else{
+        sprintf(output, "Number of OpenCL platforms: %d", platformCount);
+        verbose(output);
+    }
+    verbose("");
+
+
+    // Get devices for each platform
+    cl_device_id *devices;
+    cl_uint deviceCount;
+
+    for(cl_uint i=0; i< platformCount; i++){
+        sprintf(output, " PLATFORM %d ",i+1);
+        verboseBox(output);
+
+        devices = getDevices(platforms[i], &deviceCount);
+
+        if(deviceCount==0) verbose("No OpenCL device available for this platform.");
+        for(cl_uint j=0; j< deviceCount; j++){
+            cl_uint k=1;
+            verboseList(j+1, k++, deviceInfoFieldLabel(CL_DEVICE_NAME), getDeviceName(devices[j]));
+            verboseList(j+1, k++, deviceInfoFieldLabel(CL_DEVICE_VERSION), getDeviceHardwareVersion(devices[j]));
+            verboseList(j+1, k++, deviceInfoFieldLabel(CL_DRIVER_VERSION), getDeviceSoftwareVersion(devices[j]));
+            verboseList(j+1, k++, deviceInfoFieldLabel(CL_DEVICE_TYPE), getDeviceType(devices[j]));
+            sprintf(output, "%u", getDeviceCores(devices[j]));
+            verboseList(j+1, k++, deviceInfoFieldLabel(CL_DEVICE_MAX_WORK_GROUP_SIZE), output);
+            sprintf(output, "%u MHz", getDeviceFrequency(devices[j]));
+            verboseList(j+1, k++, deviceInfoFieldLabel(CL_DEVICE_MAX_CLOCK_FREQUENCY), output);
+        }
+    }
+}
---
+++ src/util/clUtils/interface.h
@@ -0,0 +1,11 @@
+#ifndef BENTOCL_INTERFACE_H
+#define BENTOCL_INTERFACE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <CL/cl.h>
+#include <stdbool.h>
+
+void listAllDevices();
+
+#endif
